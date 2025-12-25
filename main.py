05ae759da9b77c3b1fd891aea5f47aec95a66e55
Sagesse PC <sagesseariyanto@gmail.com>
2025-12-25 13:31:38 -0700

    extracted prices and company links

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import pandas as pd
 
 url = "https://www.futuretools.io/"
 response = requests.get(url)
@@ -9,9 +10,22 @@ companies = soup.find_all("div", class_="div-block-59")
 for company in companies:
     name = company.find("a", class_="tool-item-link---new").text
     description = company.find("div", class_="tool-item-description-box---new").text
-    market = company.find("div", class_="text-block-53").text
-    link =  urljoin(url, company.find("a", class_="tool-item-link---new")["href"])
+    category = company.find("div", class_="text-block-53").text
     upvotes = int(company.find(
         "div", class_="text-block-52 jetboost-item-total-favorites-vd2l"
     ).text)
-    print(f"Name: {name}\nDescription: {description}\nMarket: {market}\nLink: {link}\nUpvotes: {upvotes}\n")
+
+    # Get detailed page
+    link = urljoin(url, company.find("a", class_="tool-item-link---new")["href"])
+    company_response = requests.get(link)
+    company_soup = BeautifulSoup(company_response.text, 'html.parser')
+
+    # Extract additional information
+    company_link = company_soup.select_one(
+        "div.div-block-6.vertical-flex a"
+    )['href']
+    price = company_soup.select_one("div.div-block-17 div.text-block-2").text
+
+    print(
+        f"Name: {name}\nDescription: {description}\nCategory: {category}\nPrice: {price}\nUpvotes: {upvotes}\nLink: {company_link}\n"
+    )
