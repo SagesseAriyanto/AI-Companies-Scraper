ebb525cceb97eafd7f890bc3dcb406aa1db55182
Sagesse PC <sagesseariyanto@gmail.com>
2025-12-26 00:31:46 -0700

    updated code so doesn't run data collection on every run

--- main.py
+++ main.py
@@ -6,45 +6,51 @@ import pandas as pd
 seen_urls = set()           # Track what we've already processed
 company_data = []           # Store all scraped data
 
-url = "https://www.futuretools.io/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-companies = soup.find_all("div", class_="div-block-59")
-for company in companies:
-    # Check if we've already seen this URL
-    link_element = company.find("a", class_="tool-item-link---new")
-    link = urljoin(url, link_element["href"])
-    if link in seen_urls:
-        continue
-    seen_urls.add(link)
-    # Extract basic information
-    name = link_element.text
-    description = company.find("div", class_="tool-item-description-box---new").text
-    category = company.find("div", class_="text-block-53").text
-    upvotes = int(company.find(
-        "div", class_="text-block-52 jetboost-item-total-favorites-vd2l"
-    ).text)
+def get_data():
+    global seen_urls, company_data
+    url = "https://www.futuretools.io/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    companies = soup.find_all("div", class_="div-block-59")
+    for company in companies:
+        # Check if we've already seen this URL
+        link_element = company.find("a", class_="tool-item-link---new")
+        link = urljoin(url, link_element["href"])
+        if link in seen_urls:
+            continue
+        seen_urls.add(link)
+        # Extract basic information
+        name = link_element.text
+        description = company.find("div", class_="tool-item-description-box---new").text
+        category = company.find("div", class_="text-block-53").text
+        upvotes = int(company.find(
+            "div", class_="text-block-52 jetboost-item-total-favorites-vd2l"
+        ).text)
 
-    # Navigate detailed page
-    company_response = requests.get(link)
-    company_soup = BeautifulSoup(company_response.text, 'html.parser')
+        # Navigate detailed page
+        company_response = requests.get(link)
+        company_soup = BeautifulSoup(company_response.text, 'html.parser')
 
-    # Extract additional information
-    company_link = company_soup.select_one(
-        "div.div-block-6.vertical-flex a"
-    )['href']
-    price = company_soup.select_one("div.div-block-17 div.text-block-2").text
+        # Extract additional information
+        company_link = company_soup.select_one(
+            "div.div-block-6.vertical-flex a"
+        )['href']
+        price = company_soup.select_one("div.div-block-17 div.text-block-2").text
 
-    company_data.append({
-        "Name": name,
-        "Category": category,
-        "Price": price,
-        "Upvotes": upvotes,
-        "Description": description,
-        "Link": company_link
-    })
+        company_data.append({
+            "Name": name,
+            "Category": category,
+            "Price": price,
+            "Upvotes": upvotes,
+            "Description": description,
+            "Link": company_link
+        })
 
 
+get_data()
 # Save to CSV
 df = pd.DataFrame(company_data)
 df.to_csv("AI_companies.csv", index=False)
+
+data = pd.read_csv("AI_companies.csv")
+print(data.tail())
